652775e211888dc2aa94c107d09e557a158daa79
Clara <clara.marchal@edu.devinci.fr>
2025-09-24 11:55:47 +0200

    select devises

--- Convert.js
+++ Convert.js
@@ -1,9 +1,14 @@
 const apiBase = "https://v6.exchangerate-api.com/v6/";
 const apiKey = "12fd69d0539580d13bdd11c4";
+const select = document.getElementById("devises-select");
 
-fetch(apiBase + apiKey + "/latest/USD")
+
+fetch(apiBase + apiKey + "/codes")
     .then(promise => promise.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data)
+        data.supported_codes.forEach(monnaie => {
+        select.innerHTML += `<option value="${monnaie}">${monnaie}</option>`;
+        });
+    })
     .catch(error => console.error(error))
-
-// J'adore mon chat meme si il est débile
