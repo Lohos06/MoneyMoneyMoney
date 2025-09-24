1bb28485c81e9b08b462a2d92994561b5ef64864
Yasmine <meftah.yasmine2006@gmail.com>
2025-09-24 12:53:31 +0200

    js css encore

--- JS2.js
+++ JS2.js
@@ -8,5 +8,21 @@ btn.addEventListener("click", () => {
     btn.classList.toggle("red")
 });
 
+/*
 faire en sort sue si c'estnull 
-le
+le class number devient en mode rouge avec un modal qui s'affiche*/
+const form = document.querySelector('option');
+
+form.addEventListener('submit', (event) =>
+    if (input.value === "") {
+        // Si rien n'est saisi
+        input.classList.add("error"); // ajoute la bordure rouge
+        message.style.display = "block"; // affiche le message d'erreur
+    } else {
+        //enlever erreur si rien toucher
+        input.classList.remove("error");
+        message.style.display = "none";
+        alert("Montant saisi : " + input.value);
+    }
+
+});/*help*/
